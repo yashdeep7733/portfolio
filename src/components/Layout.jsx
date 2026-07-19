a0b926308283c360f8a2f84868a52298ca5bf68c
Deepyash <158832695+yashLGN@users.noreply.github.com>
2026-07-19 17:21:55 +0530

    scroll-to-top logic added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 
 export default function Layout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <Navbar />
